3e1d3c505f1ba46c59a12c71cc50cb57c1865e93
Ming <wym5413@gmail.com>
2022-10-26 10:22:02 +0200

    Homework_Noah

---
+++ your-code/functs.py
@@ -0,0 +1,135 @@
+def clean_up(s):
+    """
+    Cleans up numbers, URLs, and special characters from a string.
+
+    Args:
+        s: The string to be cleaned up.
+
+    Returns:
+        A string that has been cleaned up.
+    """
+    s=s.lower()
+    str1=''
+    s=re.sub(r'[a-zA-Z]+://[^\s]*','',s)
+    for i in re.findall('[a-z]+',s):
+        str1=str1+i+' '
+    return str1
+def clean_up(s):
+    import re
+    """
+    Cleans up numbers, URLs, and special characters from a string.
+
+    Args:
+        s: The string to be cleaned up.
+
+    Returns:
+        A string that has been cleaned up.
+    """
+    s=s.lower()
+    str1=''
+    s=re.sub(r'[a-zA-Z]+://[^\s]*','',s)
+    for i in re.findall('[a-z]+',s):
+        str1=str1+i+' '
+    return str1
+def clean_up(s):
+    import re
+    """
+    Cleans up numbers, URLs, and special characters from a string.
+
+    Args:
+        s: The string to be cleaned up.
+
+    Returns:
+        A string that has been cleaned up.
+    """
+    s=s.lower()
+    str1=''
+    s= re.sub(r'[a-zA-Z]+://[^\s]*','',s)
+    for i in re.findall('[a-z]+',s):
+        str1=str1+i+' '
+    return str1
+def clean_up(s):
+    import re
+    """
+    Cleans up numbers, URLs, and special characters from a string.
+
+    Args:
+        s: The string to be cleaned up.
+
+    Returns:
+        A string that has been cleaned up.
+    """
+    s=s.lower()
+    str1=''
+    s=re.sub(r'[a-zA-Z]+://[^\s]*','',s)
+    for i in re.findall('[a-z]+',s):
+        str1=str1+i+' '
+    return str1
+def clean_up(s):
+    import re
+    """
+    Cleans up numbers, URLs, and special characters from a string.
+
+    Args:
+        s: The string to be cleaned up.
+
+    Returns:
+        A string that has been cleaned up.
+    """
+    s=s.lower()
+    str1=''
+    s= re.sub(r'[a-zA-Z]+://[^\s]*','',s)
+    for i in re.findall('[a-z]+',s):
+        str1=str1+i+' '
+    return str1
+def clean_up(s):
+    import re
+    """
+    Cleans up numbers, URLs, and special characters from a string.
+
+    Args:
+        s: The string to be cleaned up.
+
+    Returns:
+        A string that has been cleaned up.
+    """
+    s=s.lower()
+    str1=''
+    s= re.sub(r'[a-zA-Z]+://[^\s]*','',s)
+    for i in re.findall('[a-z]+',s):
+        str1=str1+i+' '
+    return str1
+def clean_up(s):
+    import re
+    """
+    Cleans up numbers, URLs, and special characters from a string.
+
+    Args:
+        s: The string to be cleaned up.
+
+    Returns:
+        A string that has been cleaned up.
+    """
+    s=s.lower()
+    str1=''
+    s= re.sub(r'[a-zA-Z]+://[^\s]*','',s)
+    for i in re.findall('[a-z]+',s):
+        str1=str1+i+' '
+    return str1
+def clean_up(s):
+    import re
+    """
+    Cleans up numbers, URLs, and special characters from a string.
+
+    Args:
+        s: The string to be cleaned up.
+
+    Returns:
+        A string that has been cleaned up.
+    """
+    s=s.lower()
+    str1=''
+    s= re.sub(r'[a-zA-Z]+://[^\s]*','',s)
+    for i in re.findall('[a-z]+',s):
+        str1=str1+i+' '
+    return str1
